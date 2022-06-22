02456aaeb14ab186427792558370d36258a0d828
Collins Prosper <papilocollanso@gmail.com>
2022-06-21 17:14:18 -0930

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+//getting  my personal details
+//for outputting to the screen
+_name = document.getElementByID("name");
+height = document.getElementByID("height");
+country = document.getElementById("country");
+
+
+
+window.addEventListener('load',()=>{
+    
+_name.innerHTML = "Prosper Collins"
+ height.innerHTML = "80m",
+ country.innerHTML = "Canada"
+
+
+
+});
+
